3afc84a6acbcbd8a285e8d5b73871f4c79e2d96b
MM <miguelvazsilva@gmail.com>
2019-03-08 19:07:43 +0000

    Update the link for the item

--- com/miguelvazsilva/selenium/amazon/pages/ItemListPage.java
+++ com/miguelvazsilva/selenium/amazon/pages/ItemListPage.java
@@ -6,7 +6,7 @@ public class ItemListPage {
 	
 	public void clickItem(WebDriver driver) {
 		
-        	driver.findElement(By.xpath("//*[@id=\"result_0\"]/div/div[3]/div[1]/a")).click();
+        	driver.findElement(By.xpath("//*[@id=\"search\"]/div[1]/div[2]/div/span[3]/div[1]/div[1]/div/div/div/div[2]/div[2]/div/div[1]/div/div/div[1]/h5/a/span")).click();
         
 	}
     
